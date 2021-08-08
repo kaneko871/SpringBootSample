49b7566851a705dc054c10ad10c17f5111ea778c
tomo <kaneko871@gmail.com>
2021-08-08 22:01:20 +0900

    section6

--- src/main/java/com/example/controller/SignupController.java
+++ src/main/java/com/example/controller/SignupController.java
@@ -7,12 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.application.service.UserApplicationService;
+import com.example.form.GroupOrder;
 import com.example.form.SignupForm;
 
 import lombok.extern.slf4j.Slf4j;
@@ -36,7 +38,7 @@ public class SignupController {
 
 	@PostMapping("/signup")
 	public String postSignup(Model model, Locale locale,
-			@ModelAttribute SignupForm form, BindingResult bindingResult) {
+			@ModelAttribute @Validated(GroupOrder.class) SignupForm form, BindingResult bindingResult) {
 
 		if(bindingResult.hasErrors()) {
 			//NG: ユーザ登録画面に戻る
